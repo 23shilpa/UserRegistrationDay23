77bd06e12d4b3bf3d4fe31e7f1ffda845072960b
shilpa <shilpabhonde000@gmail.com>
2023-09-26 12:23:35 +0530

    UC5 Complete.

---
+++ UserRegistrationDay23/src/main/java/welcometouserregistration/PasswordValidator.java
@@ -0,0 +1,24 @@
+package welcometouserregistration;
+
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class PasswordValidator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Lambda expression to validate a password
+        Predicate<String> passwordValidator = password -> password.length() >= 8;
+
+        System.out.print("Enter a password: ");
+        String password = scanner.nextLine();
+
+        if (passwordValidator.test(password)) {
+            System.out.println("Valid password");
+        } else {
+            System.out.println("Invalid password");
+        }
+
+        scanner.close();
+    }
+}
